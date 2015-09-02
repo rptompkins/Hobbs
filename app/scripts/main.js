0479570c0b00787a9c44f48d33fa01bbbdf3a21a
rptompkins <ripoto3@gmail.com>
2015-09-01 18:42:09 -0700

    index changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
 $(document).ready(function(){
+	var nav = $('.navbar');
 	$('#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
