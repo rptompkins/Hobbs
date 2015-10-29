9cd5bc99dbf3b77eea1aaf23a5c2b7c9c04fa7c2
rptompkins <ripoto3@gmail.com>
2015-10-28 20:45:57 -0700

    almost complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,19 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
 $(document).ready(function(){
 	var nav = $('.navbar');
 	$('#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        // window.location.hash = target;
+	    });
+	});
 });
+
